0deb62ae71f054244eaec22b99a152c95d6207f8
harsha-ops <71168165+harsha-ops@users.noreply.github.com>
2024-10-16 15:09:31 +0100

    Create deploy_setup_amazonlinux.sh

---
+++ deploy_setup_amazonlinux.sh
@@ -0,0 +1,34 @@
+sudo setsebool -P httpd_can_network_connect 1
+
+# mysql -uroot < ./dbscript/studentapp.sql
+
+# Modifying the managers context.xml file to access the managers application in tomcat
+
+cp ./tomcat/manager/context.xml /opt/appserver/webapps/manager/META-INF/
+
+# Adding user to the tomcat
+
+cp ./tomcat/conf/tomcat-users.xml /opt/appserver/conf/
+
+# Loading db driver to the tomcat to integrate with db
+
+cp ./tomcat/lib/mysql-connector.jar /opt/appserver/lib/
+
+# Integrating tomcat with db
+
+cp ./tomcat/conf/context.xml /opt/appserver/conf/
+
+sudo systemctl restart tomcat
+
+
+# Deploying static application
+
+# sudo rm -rf /usr/share/nginx/html/*
+
+# sudo cp -R /opt/static-project/iPortfolio/* /usr/share/nginx/html/
+
+# Reverse proxy config
+
+# sudo cp /opt/student-app/nginx/nginx.conf /etc/nginx/
+
+# sudo systemctl restart nginx
